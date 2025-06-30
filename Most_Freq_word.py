a78cf5405d7ede28a5f3757b92b5c47ef85af5a9
Pratikbamane2707 <pratikbamane200@gmail.com>
2025-07-01 01:48:09 +0530

    Added new files Most frequest word search and file for it

---
+++ Most_Freq_word.py
@@ -0,0 +1,19 @@
+file = input('Enter file name or path:')
+
+dictt ={}
+with open(file,'r') as file:
+    content = file.read()
+    content=content.lower()
+    for i in content.split(' '):
+        if i.lower() not in dictt:
+            dictt[i]=1
+        else:
+            dictt[i]+=1
+
+def find_MaxKey(dictt):
+    max_val=max(dictt.values())
+    for i in dictt.items():
+        if i[1]==max_val:
+            return i
+max_key=find_MaxKey(dictt)
+print(f'Most frequent word: {max_key[0]} (Count: {max_key[1]})')
