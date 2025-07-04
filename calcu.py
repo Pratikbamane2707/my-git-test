be2f764b9fb57b28ebfd76d0d6a5128518fdf310
Pratikbamane2707 <pratikbamane200@gmail.com>
2025-07-04 18:12:11 +0530

    Renamed choice to operation

--- calcu.py
+++ calcu.py
@@ -1,17 +1,17 @@
 import sys
 print("Enter the choice in between 1 to 4 \n 1-add \n 2-Sub \n 3-multiply \n 4-divide ")
-choice = int(sys.argv[1])
+operation = int(sys.argv[1])
 num1=float(sys.argv[1])
 num2=float(sys.argv[2])
 
-if choice==1:
+if operation==1:
     print(f'The addition of {num1} and {num2} is {num1+num2}')
 
-elif choice==2:
+elif operation==2:
     print(f'The Substraction of {num1} and {num2} is {num1-num2}')
-elif choice==3:
+elif operation==3:
     print(f'The Multiplication of {num1} and {num2} is {num1*num2}')
-elif choice==4:
+elif operation==4:
     print(f'The Division of {num1} and {num2} is {num1/num2}')
 else:
     print('Enter in between 1 to 4 you are entering wrong')
