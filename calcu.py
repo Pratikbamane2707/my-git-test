39206d5647cc5b0c8d469ad26ca667626c9b65a3
Pratikbamane2707 <pratikbamane200@gmail.com>
2025-07-16 00:03:11 +0530

    Added squaring feature

--- calcu.py
+++ calcu.py
@@ -1,5 +1,5 @@
 import sys
-print("Enter the choice in between 1 to 4 \n 1-add \n 2-Sub \n 3-multiply \n 4-divide ")
+print("Enter the choice in between 1 to 4 \n 1-add \n 2-Sub \n 3-multiply \n 4-divide  \n 5-square")
 operation = int(sys.argv[1])
 num1=float(sys.argv[2])
 num2=float(sys.argv[3])
@@ -13,5 +13,8 @@ elif operation==3:
     print(f'The Multiplication of {num1} and {num2} is {num1*num2}')
 elif operation==4:
     print(f'The Division of {num1} and {num2} is {num1/num2}')
+elif operation==5:
+    print(f'The exponential multiplication of numbers is -- {num1} raise to {num2} is {num1**num2}')
+
 else:
     print('Enter in between 1 to 4 you are entering wrong')
